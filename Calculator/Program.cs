08f34636577ec607e0d5bd5cad84f4c3b5138c23
Ilya <vik.kor.1917@mail.ru>
2017-08-04 15:35:20 +0300

    initial commit

---
+++ Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorClass;
+
+namespace CalculatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+
+            var input = Console.ReadLine().Replace('.', ',');
+            string[] numbers = input.Split(new char[] { '-', '+', '*', '/' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] operators = input.Split(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //массив строк,в котором будет разбитая строка input
+            string[] statements = new string[numbers.Length + operators.Length];
+
+            calculator.FillingString(statements, numbers, operators);
+
+            calculator.MultDiv(statements);
+
+            calculator.AddSubtr(simpleStatement);
+
+
+            Console.ReadKey();
+
+
+            //Console.WriteLine(input + "=" + n[n.Length - 1]);
+        }
+    }
+}
---
+++ CalculatorClass/Calculator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+namespace CalculatorClass
+{
+    public class Calculator
+    {
+        public string ParseInput(string input)
+        {
+            return input;
+        }
+
+        //заполнение массива строк[] по порядку, как в вводимом выражении
+        public string[] FillingString(string[] statements, string[] numbers, string[] operators)
+        {
+            int iteration = 0;
+            for (int i = 0, j = 1, k = 0; i < statements.Length; i += 2, j += 2, k++)
+            {
+                statements[i] = numbers[k];
+                if (iteration < operators.Length) statements[j] = operators[k];
+                iteration++;
+            }
+            return statements;
+        }
+
+        //Выполняет действия "*,/", другие не учитываются
+        public string[] MultDiv(string[] statements)
+        {
+            int countnull = 0;
+            for (int i = 1; i < statements.Length - 1; i++)
+            {
+                if (statements[i] == "*")
+                {
+                    statements[i - 1] = Convert.ToString(Convert.ToDouble(statements[i - 1]) * Convert.ToDouble(statements[i + 1]));
+                    statements[i] = null;
+                    statements[i + 1] = null;
+                    countnull += 2;
+                }
+                if (statements[i] == "/")
+                {
+                    statements[i - 1] = Convert.ToString(Convert.ToDouble(statements[i - 1]) / Convert.ToDouble(statements[i + 1]));
+                    statements[i] = null;
+                    statements[i + 1] = null;
+                    countnull += 2;
+                }
+            }
+            //создаём массив без символов null
+            string[] simpleStatement = new string[statements.Length - countnull];
+            //заполняем массив символами
+            for (int i = 0, j = 0; i < statements.Length; i++)
+            {
+                if (statements[i] != null)
+                {
+                    simpleStatement[j] = statements[i];
+                    j++;
+                }
+            }
+            return simpleStatement;
+        }
+
+        //Вычисляет простое выражение(при вхоящих знаках "+,-") из входящего массива строк
+        public string[] AddSubtr(string[] simpleStatement)
+        {
+            for (int i = 1; i < simpleStatement.Length - 1; i++)
+            {
+                if (simpleStatement[i] == "+")
+                {
+                    simpleStatement[i + 1] = Convert.ToString(Convert.ToDouble(simpleStatement[i - 1]) + Convert.ToDouble(simpleStatement[i + 1]));
+                    simpleStatement[i - 1] = null;
+                    simpleStatement[i] = null;
+                }
+                if (simpleStatement[i] == "-")
+                {
+                    simpleStatement[i + 1] = Convert.ToString(Convert.ToDouble(simpleStatement[i - 1]) - Convert.ToDouble(simpleStatement[i + 1]));
+                    simpleStatement[i - 1] = null;
+                    simpleStatement[i] = null;
+                }
+            }
+            return simpleStatement;
+        }
+
+        public string OutputResult(string[] simpleStatement)
+        {
+            return simpleStatement[simpleStatement.Length - 1];
+        }
+
+
+
+
+    }
+}
